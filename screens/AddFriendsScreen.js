472a1bdbe0eabe992ac56c5a5a01912adc194be6
reece346 <volticeagle@hotmail.com>
2022-12-02 02:18:47 -0500

    Started the Profile Cards for Add Friends

--- screens/AddFriendsScreen.js
+++ screens/AddFriendsScreen.js
@@ -1,87 +1,162 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {Image,StyleSheet,Text,View,TouchableOpacity, TextInput, Button} from 'react-native';
+//import { useState } from 'react';
+import {FlatList,Image,StyleSheet,Text,View,TouchableOpacity, TextInput, Button, SafeAreaView} from 'react-native';
 
-export default function AddFriendsScreen(){
-    
+const suggestedFriendsDATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'Reece Peters',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Bea Dyar',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Miller Kershaw',
+    },
+];
+
+const pendingFriendsDATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'Ronan Stewart',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'John Smith',
+    },
+];
+
+const Item = ({ title }) => (
+    <View style={styles.ProfileCards}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
+
+const AddFriendsScreen = () => {
+
     const toggleModal = () => {
         setModalVisible(!modalVisible)
     }
 
+    const renderItem = ({ item }) => (
+        <Item title={item.title} />
+    );
+
     return (
-        <View style={styles.container}>
-            <View style={styles.SearchFriendHeader}>
-                <Text style={{fontSize: 25, color: 'white', fontWeight: '600'}}>
-                    Search Users
-                </Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.SearchFriendsSection}>
+                <View style={styles.SearchFriendHeader}>
+                    <Text style={{fontSize: 25, color: 'white', fontWeight: '600'}}>
+                        Search Users
+                    </Text>
+                    <View style={{ position: 'absolute', marginVertical: 30, width: '100%'}}>
+                        <TextInput style={{
+                            borderRadius: 10,
+                            margin: 10,
+                            color: '#000',
+                            borderColor: '#666',
+                            backgroundColor: '#FFF',
+                            borderWidth: 1,
+                            height: 45,
+                            paddingHorizontal: 10,
+                            fontSize: 18,
+                        }}
+                        placeholder={'Username'}
+                        placeholderTextColor={'#666'}
+                        />
+                    </View>
+                </View>
+                <TouchableOpacity onPress= {toggleModal} style={{flexDirection: 'row', alignSelf: 'center', }}>
+                    <View style={styles.AddButton}>
+                        <Text style={{color: 'white', top: 4}}>Search</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
-            <View style={{ position: 'absolute', top: 75, width: '90%'}}>
-                <TextInput style={{
-                        left: 20,
-                        borderRadius: 10,
-                        margin: 10,
-                        color: '#000',
-                        borderColor: '#666',
-                        backgroundColor: '#FFF',
-                        borderWidth: 1,
-                        height: 45,
-                        paddingHorizontal: 10,
-                        fontSize: 18,
-                    }}
-                    placeholder={'Username'}
-                    placeholderTextColor={'#666'}
+            
+            <View style={styles.SuggestedFriendsSection}>
+                <View style={styles.SuggestedFriendsHeader}>
+                    <Text style={{fontSize: 25, color: 'white', fontWeight: '600'}}>
+                        Suggested Friends
+                    </Text>
+                </View>
+                <FlatList
+                    data={suggestedFriendsDATA}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
                 />
             </View>
-            <TouchableOpacity onPress={toggleModal} style={{flexDirection: 'row', justifyContent: 'center', alignContent: 'center'}}>
-                <View style={styles.AddButton}>
-                    <Text style={{color: 'white', top: 4}}>Search</Text>
-                </View>
-            </TouchableOpacity>
-            <View style={styles.SuggestedFriendsHeader}>
-                <Text style={{fontSize: 25, color: 'white', fontWeight: '600'}}>
-                    Suggested Friends
-                </Text>
+
+            <View style={styles.PendingFriendsSection}>
+                <View style={styles.PendingFriendsHeader}>
+                    <Text style={{fontSize: 25, color: 'white', fontWeight: '600'}}>
+                        Pending Friends
+                    </Text>
+                </View> 
+                <FlatList
+                    data={pendingFriendsDATA}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                />
             </View>
-            <View style={styles.PendingFriendsHeader}>
-                <Text style={{fontSize: 25, color: 'white', fontWeight: '600'}}>
-                    Pending Friends
-                </Text>
-            </View>
-        </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container : {
         flex:1,
-        backgroundColor: '#3A3B50'
+        backgroundColor: '#3A3B50',
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    SearchFriendsSection : {
+        top: 20,
+        marginHorizontal: 30
     },
     SearchFriendHeader : {
-        top: 20,
-        left: 30,
-        flexDirection: 'row',
+        flexDirection: 'row'
+    },
+    ProfileCards : {
+        backgroundColor: '#ffffff',
+        top: 10,
+        height: 75,
+        width: 352,
+        padding: 10,
+        marginVertical: 8,
+    },
+    SuggestedFriendsSection : {
+        marginVertical: 60,
+        left: 30
     },
     SuggestedFriendsHeader : {
-        top: 115,
-        left: 30,
         flexDirection: 'row'
-        
     },
     AddButton : {
-        top: 100,
+        marginVertical: 30,
         justifyContent: 'center',
         alignContent: 'center',
-        margin: 10,
         backgroundColor: '#5D5F82',
         borderRadius: 5,
         height: 25,
         width: 75,
-        flexDirection: 'row'
+        flexDirection: 'row',
+        top: 40
+    },
+    PendingFriendsSection : {
+        marginVertical: 0,
+        left: 30
     },
     PendingFriendsHeader : {
-        top: 350,
-        left: 30,
         flexDirection: 'row'
-        
+    },
+    title : {
+        left: '25%',
+        fontWeight: 'bold',
+        fontSize: 20,
     }
 })
+
+
+export default AddFriendsScreen;
