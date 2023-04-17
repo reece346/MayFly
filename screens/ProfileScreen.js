0882f709a780b8a53a43f4aecc7a52aa88b6db70
RonanMStewart <70454075+RonanMStewart@users.noreply.github.com>
2023-04-17 16:11:48 -0400

    fixed style on profileSCreen to use percentages instead of absolute position

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -12,142 +12,100 @@ export default function Profile() {
     numberOfFriends = getActiveUser().friendIDs.length-1;
     return (
         <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <TouchableOpacity onPress={() => {RootNavigation.navigate("EditProfile")}}>
-                    <Image style={{height:20, width:20, color: 'black', left: '315', top: 10, left: 300}} source={require('./images/editProfileIcon.png')}/>
-                </TouchableOpacity>
-                <View style={styles.avatarBorderContainer}>
-                    <Image style={{height:'100%', width:'100%', borderRadius:70, color: 'black'}} source={require('./images/blackCircleBorder.jpg')}/>
-                </View>
-                <View style={styles.avatarContainer}>
-                    <Image style={{height:'100%', width:'100%', borderRadius:60}} source={require('./images/standardpfp.png')}/>
-                </View>
-                <View style={styles.profileNameContainer}>
-                   <Text style={{fontSize:20,fontWeight:'bold'}}>
-                        {displayNameSaved}
-                    </Text>
-                </View>
-                <View style={styles.userNameContainer}>
-                   <Text style={{fontSize:16,}}>
-                        {displayNameSaved}
-                    </Text>
-                </View>
-                <View style={styles.MayFlySinceContainer}>
-                   <Text style={{fontSize:15,}}>
-                        Friends:
-                    </Text>
-                    <Text style={{fontSize:15, left: 25}}>
-                        {numberOfFriends}
-                    </Text>
+            <View style={styles.editProfile}>
+                <View style={styles.imageContainer}>
+                    <View style={styles.avatarContainer}>
+                        <Image style={{height:60, width:60, borderWidth:2, borderRadius: 60, borderColor: 'grey'}} source={require('./images/standardpfp.png')}/>
+                    </View>
+                    <View style={styles.profileNameContainer}>
+                        <Text style={{fontSize:20,fontWeight:'bold'}}>
+                            {displayNameSaved}
+                        </Text>
+                        <Text style={{fontSize:16,}}>
+                            {displayNameSaved}
+                        </Text>
+                    </View>
+                    <View style={styles.MayFlySinceContainer}>
+                        <Text style={{fontSize:15,}}>
+                            Friends: {numberOfFriends}
+                        </Text>
+                    </View>
+                    <TouchableOpacity onPress={() => {RootNavigation.navigate("EditProfile")}}>
+                        <Image style={{height:20, width:20, color: 'black'}} source={require('./images/editProfileIcon.png')}/>
+                    </TouchableOpacity>
                 </View>
             </View>
             <View style={styles.InterestsHeader}>
-                <Text style={{fontSize: 25, color: 'white', fontWeight: '600'}}>
+                <Text style={{fontSize: 25, color: 'white', fontWeight: '600', paddingBottom: 15}}>
                      Interests
-
                 </Text>
-            </View>
-            <View style={styles.InterestsHeader}>
-                <Text style={{fontSize: 25, color: 'white',fontWeight: '600'}}>
+                <Text style={{fontSize: 25, color: 'white',fontWeight: '600', marginLeft: '5%'}}>
                      {interestArray}
                 </Text>
             </View>
+            
             <View style={styles.FriendsHeader}>
-            <TouchableOpacity onPress={() => {RootNavigation.navigate("AddFriends")}}>
-                    <Image style={{height:20, width:20, color: 'black', left: 315, top: 30}} source={require('./images/addfriends.png')}/>
-                </TouchableOpacity>
-                <Text style={{fontSize: 25, color: 'white', fontWeight: '600'}}>
-                    Favorite Friends
-                </Text>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <Text style={{fontSize: 25, color: 'white', fontWeight: '600'}}>
+                        Favorite Friends
+                    </Text><TouchableOpacity onPress={() => {RootNavigation.navigate("AddFriends")}}>
+                        <Image style={{height:25, width:25, color: 'black'}} source={require('./images/addfriends.png')}/>
+                    </TouchableOpacity>
+                </View>
             </View>
 
-            <View style={styles.InstagramButton}>
-                <Button title='Instagram' onPress={()=>{Linking.openURL('https://www.instagram.com/')}}></Button>
+            <View style={styles.linksContainer}>
+                <View style={styles.InstagramButton}>
+                    <Button title='Instagram' onPress={()=>{Linking.openURL('https://www.instagram.com/')}}></Button>
+                </View>
+                <View style={styles.TwitterButton}>
+                    <Button title='Twitter' onPress={()=>{Linking.openURL('https://twitter.com/')}}></Button>
+                </View>
+                <View style={styles.YoutubeButton}>
+                    <Button title='Youtube' onPress={()=>{Linking.openURL('https://www.youtube.com/')}}></Button>
+                </View>
             </View>
-            <View style={styles.TwitterButton}>
-                <Button title='Twitter' onPress={()=>{Linking.openURL('https://twitter.com/')}}></Button>
-            </View>
-            <View style={styles.YoutubeButton}>
-                <Button title='Youtube' onPress={()=>{Linking.openURL('https://www.youtube.com/')}}></Button>
-            </View>
-            </View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex: 3,
         backgroundColor: '#3A3B50',
+        flex: 1,
+    },
+    editProfile:{
+        width : '85%',
+        marginLeft: '7.5%',
+        marginVertical : '5%',
+        alignItems: 'center',
+        backgroundColor: '#EAEAEA',
+        borderRadius: 20,
+        flex: 2,
     },
     imageContainer:{
-        left: 30,
-        top:30,
-        height:150,
-        width:350,
-        backgroundColor:'#EAEAEA',
-        borderRadius:22,
-        elevation:2
-    },
-    avatarBorderContainer:{
-        height:110,
-        width:110,
-        position:'absolute',
-        left: 15,
-        top: 20
+        width: '100%',
+        height: '100%',
+        position: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 15
     },
     avatarContainer:{
-        height:100,
-        width:100,
-        position:'absolute',
-        left: 20,
-        top: 25
-    },
-    profileNameContainer:{
-        position:'absolute',
-        top: 30,
-        left: 135,
-
-        color: 'black'
-    },
-    userNameContainer:{
-        position:'absolute',
-        top: 58,
-        left: 145,
-        
-    },
-    MayFlySinceContainer:{
-        position:'absolute',
-        top: 85,
-        left: 138,
-        alignItems:'flex',
+        marginHorizontal: 10,
     },
     InterestsHeader:{
-        top: 50,
-        left: 30
+        flex: 5,
+        paddingLeft: '5%',
     },
-
     FriendsHeader:{
-        top: 200,
-        left: 30
+        flex: 5,
+        paddingLeft: '5%',
+        paddingRight : '5%'
     },
-
-    InstagramButton:{
-        position:'absolute',
-
-        top: 630,
-        left: 30
+    linksContainer:{
+        flex: 2,
+        flexDirection: 'row',
+        justifyContent: 'center'
     },
-    TwitterButton:{
-        position:'absolute',
-        
-        top: 630,
-        left: 173
-
-    },
-    YoutubeButton:{
-        position:'absolute',
-        
-        top: 630,
-        left: 300
-    }
 });
