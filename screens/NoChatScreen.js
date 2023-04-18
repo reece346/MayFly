3b594a480cd7a4ed7d999b181446e72ec654543b
reece346 <volticeagle@hotmail.com>
2023-04-17 23:40:22 -0400

    Removed Add Friends from Ham Menu (closes #153)

--- screens/NoChatScreen.js
+++ screens/NoChatScreen.js
@@ -3,7 +3,7 @@ import DropDown from '../DropDown';
 import { useEffect, useState } from 'react';
 
 
-let hamburger =[{id:1, name: 'View Profile'}, {id:2, name:'Friends'}, {id:3, name:'Add Friends'}, {id:4, name:'Logout'}]
+let hamburger =[{id:1, name: 'View Profile'}, {id:2, name:'Friends'}, {id:3, name:'Logout'}]
 
 export default function NoChatScreen() {
     const [selectedItem, setSelectedItem] = useState(null)
